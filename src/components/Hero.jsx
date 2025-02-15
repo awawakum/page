9b4076b096fa882f14b0cfdcde7e22ad84cdaa1d
awawakum <mr.hatenov@gmail.com>
2025-02-15 14:27:24 +0300

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
         {/* Название проекта */}
         <h1
           style={{
-            fontSize: 'clamp(4rem, 5vw, 6rem)', // Уменьшаем максимальный размер для iPhone
+            fontSize: 'clamp(6rem, 7vw, 8rem)', // Уменьшаем максимальный размер для iPhone
             fontWeight: 'bold',
             margin: 0,
             lineHeight: 1.2, // Увеличиваем line-height для лучшей читаемости
